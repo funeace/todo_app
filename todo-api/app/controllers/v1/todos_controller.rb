0b8bb0dd52b9a443fdea75a90295779625b45580
vagrant <vagrant@localhost.localdomain>
2020-06-03 04:51:40 +0000

    [Add]Controllers

---
+++ todo-api/app/controllers/v1/todos_controller.rb
@@ -0,0 +1,24 @@
+class V1::TodosController < ApplicationController
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      # status: で返却するステータスを明示的に指定している
+      render json: todo, status: :created
+    else
+      render json: todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    if todo.destroy
+      render json: todo
+    end
+  end
+end
+
+private
+  def todo_params
+    params.require(:todo).permit(:title, :user_id)
+  end
+end
